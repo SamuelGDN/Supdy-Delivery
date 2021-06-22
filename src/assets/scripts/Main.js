7d724888df26c62af507cff8bcd7fba3acf83e35
gabriel.gomes <gomes.hirano@gmail.com>
2021-06-22 16:41:13 -0300

    Json engatinhando

--- src/assets/scripts/Main.js
+++ src/assets/scripts/Main.js
@@ -1,7 +1,22 @@
-fetch('http://localhost/del/src/assets/scripts/ofertas.json')
+fetch('http://localhost/del/src/assets/scripts/Ofertas.json')
 	.then(function (resp) {
 		return resp.json();
 	})
 	.then(function(data){
-		console.log(data)
+		for(var i = 0; i < 6;i++){
+			var el = document.getElementById(`oferta-${i}`);
+			var texto = el.children[1].children[0];
+			var preco = el.children[1].children[1].children[0];
+			var image = el.children[0];
+			console.log(texto);
+			texto.innerHTML = data.cervejas[i]['description'];
+			preco.innerHTML = data.cervejas[i]['preco'];
+			image.src = data.cervejas[i]['img'];
+			
+		}
+
+
 	});
+
+
+	
